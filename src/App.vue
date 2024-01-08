5d32389c11818a0547aa3663b129286569ab8067
Alfredo Castrogiovanni <castro.alfry@gmail.com>
2024-01-08 10:29:12 +0100

    create scaffolding

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+    import AppHeader from './components/AppHeader.vue';
+    import AppMain from './components/AppMain.vue';
+    import AppFooter from './components/AppFooter.vue';
+
+    export default {
+    name: "AppVue",
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+    }
+    }
+</script>
+
+<template>
+    <AppHeader/>
+    <AppMain/>
+    <AppFooter/>
+</template>
+
+<style lang="scss">
+    @use './scss/general.scss' as *;
+</style>
